7b292cc5bf89a97d03b42eeb280bc3a39c8e05dc
Jan Peter Axelsson <jan.peter.axelsson@icloud.com>
2024-08-29 11:15:18 +0200

    Updated with new Linux FMU OM 1.23.1

--- BPL_CHO_perfusion_cspr_openloop_explore.py
+++ BPL_CHO_perfusion_cspr_openloop_explore.py
@@ -86,7 +86,7 @@ elif platform.system() == 'Linux':
    flag_vendor = 'OM'
    flag_type = 'ME'
    if flag_vendor in ['OM','om']:
-      print('Linux - run FMU pre-comiled OpenModelica 1.23.0-dev') 
+      print('Linux - run FMU pre-comiled OpenModelica') 
       if flag_type in ['CS','cs']:         
          fmu_model ='BPL_CHO_Perfusion_cspr_openloop_linux_om_cs.fmu'    
          model = load_fmu(fmu_model, log_level=0) 
@@ -118,7 +118,7 @@ if flag_vendor in ['JM', 'jm']:
 elif flag_vendor in ['OM', 'om']:
    MSL_usage = '3.2.3 - used components: RealInput, RealOutput, CombiTimeTable, Types' 
    MSL_version = '3.2.3'
-   BPL_version = 'Bioprocess Library version 2.2.0' 
+   BPL_version = 'Bioprocess Library version 2.2.1 - GUI' 
 else:    
    print('There is no FMU for this platform')
 
