17bea44967eb5fc9b13e3d105a6ad7fc0bf7bb51
Jan Peter Axelsson <jan.peter.axelsson@icloud.com>
2025-11-19 13:50:36 +0100

    Chaged FMU-explore parLocation() to have sheets as argument

--- BPL_CHO_perfusion_cspr_openloop_explore_fmpy.py
+++ BPL_CHO_perfusion_cspr_openloop_explore_fmpy.py
@@ -30,6 +30,7 @@
 # 2025-11-09 - FMU-explore 1.0.2
 # 2025-11-13 - Test FMU-explore 1.0.1h and global declaration removed outside functions
 # 2025-11-14 - FMU-explore 1.0.2 corrected
+# 2025-11-19 - FMU-explore 1.0.2 corrected again parLocation() with sheets as argument
 #-------------------------------------------------------------------------------------------------------------------
 
 # Setup framework
@@ -521,7 +522,7 @@ def readParValue(file, sheet, parValue=parValue):
    parValue.update(parValue_local)
 
 # Define how to read dictionary for parameter location
-def readParLocation(file, parLocation=parLocation):
+def readParLocation(file, sheets, parLocation=parLocation):
    """ Read parameter short and long names from an Excel-file sheet by sheet. For use in the notebook!
        Return a dictionary."""
    sheets = ['initial_values','feed_AB', 'feed_G', 'culture', 'broth_decay']
